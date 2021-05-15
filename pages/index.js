c391db9e162f899fc85f66f9a62e2a5e8db19309
Vrishtrix <vrishin.u@gmail.com>
2021-05-15 14:29:52 +0000

    [func] Implemented function to send the messages.

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,24 @@ import Head from 'next/head';
 import { useState } from 'react';
 import { instance } from '..';
 
-export default () => {
+const sendMessage = async(message, sendList) => {
+      for (const contact in sendList) {
+            await instance.post('chat.sendMessage', {
+                  token: process.env.BOT_TOKEN,
+                  to: contact[0],
+                  text: 'Hey there! You have a new message.',
+                  attachments: [{
+                        title: 'Message from Upesh Patel',
+                        description: '',
+                        views: {
+                              flockml: '<flockml>' + message.replace('\r\n', '<br />').replace('{name}', contact[1]) + '</flockml>'
+                        }
+                  }]
+            });
+      }
+}
+
+const Home = () => {
       const [ message, updateMessage ] = useState('');
       const [ contacts, updateContacts ] = useState([]);
 
@@ -27,7 +44,7 @@ export default () => {
 
 export const getServerSideProps = async() => {
       const contactList = await instance.post('roster.listContacts', {
-            token: process.env.TOKEN
+            token: process.env.USER_TOKEN
       })
       .then(res => res.data);
 
@@ -37,3 +54,5 @@ export const getServerSideProps = async() => {
             }
       }
 }
+
+export default Home;
