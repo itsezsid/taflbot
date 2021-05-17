dec6b5d6dd3f9de1fc004c208d3356ed890b7d4d
Vrishtrix <vrishin.u@gmail.com>
2021-05-17 14:18:56 +0000

    [api] Route for sending messages

---
+++ pages/api/sendmessage.js
@@ -0,0 +1,22 @@
+const handler = (req, res) => {
+      const { message, sendList } = req.body;
+
+      for (const contact in sendList) {
+            await instance.post('chat.sendMessage', {
+                  token: process.env.BOT_TOKEN,
+                  to: contact[0],
+                  text: 'Hey there! You have a new message.',
+                  attachments: [{
+                        title: 'Message from Upesh Patel',
+                        description: '',
+                        views: {
+                              flockml: '<flockml>' + message.replace('\r\n', '<br />').replace('{name}', contact[1]) + '</flockml>'
+                        }
+                  }]
+            })
+            .then(res => console.log(res))
+            .catch(err => console.error(err));
+      }
+}
+
+export default handler;
